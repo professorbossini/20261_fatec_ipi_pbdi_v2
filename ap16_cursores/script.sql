9ad93a91339fe0b5a8caa0e87069005c729b677b
Rodrigo Bossini <professorbossini@gmail.com>
2026-03-24 11:25:13 -0300

    learn(ap16): encrerra codigo 2.7.1
    - codigo 2.8 proposto como exercicio, retomar fazendo e terminando a apostila

---
+++ ap16_cursores/script.sql
@@ -0,0 +1,22 @@
+
+
+-- DO $$
+-- DECLARE
+--  --1. Declaração do cursor
+--  --não vin
+--  cur_nomes_youtubers REFCURSOR;
+-- BEGIN
+
+-- END;
+-- $$
+-- SELECT * FROM tb_top_youtubers;
+-- CREATE TABLE tb_top_youtubers(
+-- 	cod_top_youtubers SERIAL PRIMARY KEY,
+-- 	rank INT,
+-- 	youtuber VARCHAR(200),
+-- 	subscribers INT,
+-- 	video_views VARCHAR(200),
+-- 	video_count INT,
+-- 	category VARCHAR(200),
+-- 	started INT
+-- );
