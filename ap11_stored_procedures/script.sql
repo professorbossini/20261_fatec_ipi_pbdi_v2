d646d47c52ab4c8577c162271a0acb9fcc9bf60c
Rodrigo Bossini <professorbossini@gmail.com>
2026-05-19 08:20:09 -0300

    learn(ap11): encerra apostila

--- ap11_stored_procedures/script.sql
+++ ap11_stored_procedures/script.sql
@@ -1,3 +1,107 @@
+
+
+
+CREATE OR REPLACE PROCEDURE sp_calcular_troco(
+  OUT troco INT,
+  IN valor_a_pagar INT,
+  IN valor_total INT  
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  troco := valor_a_pagar - valor_total;  
+END;
+$$
+
+
+-- DO $$
+-- BEGIN
+--   CALL sp_fechar_pedido(200, 1);
+-- END;
+-- $$
+
+-- SELECT * FROM tb_pedido;
+
+--fechar pedido
+--informar valor a pagar em modo IN
+--e codigo do pedido em modo IN
+--chamar a proc para verificar o valor total do pedido
+--se o valor que chegou via parâmetro for suficiente, fechar
+--caso contrário, dizer que não é suficiente
+-- CREATE OR REPLACE PROCEDURE sp_fechar_pedido(
+--   IN valor_a_pagar INT,
+--   IN cod_pedido INT
+-- ) LANGUAGE plpgsql
+-- AS $$
+-- DECLARE
+--   valor_total INT;
+-- BEGIN
+--   CALL sp_calcular_valor_de_um_pedido(cod_pedido, valor_total);
+--   IF valor_a_pagar < valor_total THEN
+--     RAISE 'R$% insuficiente para pagar a conta de R$%', valor_a_pagar, valor_total;
+--   ELSE
+--     UPDATE tb_pedido p SET
+--     data_modificacao = CURRENT_TIMESTAMP,
+--     status = 'fechado'
+--     WHERE p.cod_pedido = $2;
+--   END IF;
+-- END;
+-- $$
+
+
+
+
+--calcular valor de um pedido, ou seja, somar o valor de todos os seus itens
+--disponibilizar o valor total numa variável em modo OUT
+--o parâmetro que chega em modo IN é o código do pedido
+
+DO $$
+DECLARE
+  valor_total INT;
+BEGIN
+  CALL sp_calcular_valor_de_um_pedido(1, valor_total);
+  RAISE NOTICE 'Total do pedido %: R$%', 1, valor_total;
+END;
+$$
+
+CREATE OR REPLACE PROCEDURE sp_calcular_valor_de_um_pedido(
+  IN p_cod_pedido INT,
+  OUT valor_total INT
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+  SELECT SUM(valor) FROM
+  tb_pedido p
+  INNER JOIN tb_item_pedido ip ON
+  p.cod_pedido = ip.cod_pedido
+  INNER JOIN tb_item i ON
+  i.cod_item = ip.cod_item
+  WHERE p.cod_pedido = $1
+  INTO $2;
+END;
+$$
+
+SELECT * FROM tb_item;
+
+CALL sp_adicionar_item_a_pedido(3, 1);
+-- SELECT * FROM tb_item_pedido;
+-- SELECT * FROM tb_pedido;
+
+-- CREATE OR REPLACE PROCEDURE sp_adicionar_item_a_pedido(
+--   IN cod_item INT,
+--   IN cod_pedido INT
+-- ) LANGUAGE plpgsql
+-- AS $$
+-- BEGIN
+--   INSERT INTO tb_item_pedido
+--     (cod_item, cod_pedido)
+--   VALUES
+--     ($1, $2);
+--   UPDATE tb_pedido p 
+--   SET data_modificacao = CURRENT_TIMESTAMP
+--   WHERE p.cod_pedido = $2;
+-- END;
+-- $$
+-- Active: 1778585004725@@127.0.0.1@5432@20261_fatec_ipi_pbdi_rodrigo
 --escrever um proc que adiciona um item a um pedido
 --ele deve associar o item ao pedido e atualizar a data de modificação
 --parâmetros: cod_item e o cod_pedido
